697f779e3f79aedda47bdd6fe0e257a8d0370fc3
eunbi99 <silverbi99@naver.com>
2021-09-05 23:24:53 +0900

    상품 등록 폼과 상품 등록 처리

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -25,6 +24,68 @@ public class BasicItemController {
         model.addAttribute("items", items);
         return "basic/items";
     }
+
+    /* 상품 상세 정보 */
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    /**
+     * 상품 등록 폼 이동(get)과 상품 등록(post)
+     * /add라는 url은 같지만 get과 post에 따라 기능을 분리
+     */
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item",item); // 상품 등록 후 상세 페이지 이동
+        return "basic/item";
+    }
+
+    /**
+     *
+     * @ModelAttribute의 기능
+     * 1.Item 객체를 생성 후 , 요청 파라미터의 값을 프로퍼티 접근법(setXXX)로 입력.
+     * 2.Model에 ModelAttribute로 지정한 객체를 자동으로 넣어준다.
+     *
+     */
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item,Model model){
+        itemRepository.save(item);
+       // model.addAttribute("item",item); // ModelAttribute 사용시 자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item,Model model){
+        /* HelloData -> helloData
+        *  Item -> item (첫 글자를 소문자로 바꾸어서 modelAttribute에 넣어준다)
+        * */
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item,Model model){
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
